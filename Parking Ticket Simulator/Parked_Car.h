df351979d61cd261b44e3b3b6aa4448431cb8853
jmu35764 <jmu35764@email.vccs.edu>
2026-04-18 18:35:03 -0400

    Add project files.

---
+++ Parking Ticket Simulator/Parked_Car.h
@@ -0,0 +1,35 @@
+#ifndef PARKEDCAR_H
+#define PARKEDCAR_H
+
+#include <string>
+using namespace std;
+
+class ParkedCar
+{
+private:
+	string make;
+	string model;
+	string color;
+	string license_number;
+	int minutes_parked;
+
+public:
+	ParkedCar(string mk, string mdl, string clr, string ln, int m_p)
+	{
+		SetCarInfo(" ", " ", " ", " ", 0);
+	}
+
+	void SetCarInfo(string mk, string mdl, string clr, string ln, int m_pk)
+	{
+		make = mk;
+		model = mdl;
+		color = clr;
+		license_number = ln;
+		minutes_parked = m_pk;
+	}
+};
+
+
+
+#endif 
+
